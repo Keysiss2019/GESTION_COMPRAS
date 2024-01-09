fee6e1e98527a820d4c86f12c20fa430891c1907
keysiss2019 <keysiss2019@gmail.com>
2024-01-09 14:11:44 -0600

    Dejar un mismo color en los btn cancelar, modificado se llene automaticamente segun usuario  inicio de sesion.

--- categorias/editar_categoria.php
+++ categorias/editar_categoria.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -49,82 +48,69 @@
         }
 
         .custom-button {
-    display: inline-block;
-    padding: 10px 20px; /* Ajusta el tamaño del botón según tus preferencias */
-    border: none;
-    border-radius: 3px;
-    font-weight: bold;
-    cursor: pointer;
-}
+          display: inline-block;
+          padding: 10px 20px; /* Ajusta el tamaño del botón según tus preferencias */
+          border: none;
+          border-radius: 3px;
+          font-weight: bold;
+         cursor: pointer;
+        }
 
 
-.custom-button.cancel-button {
-    background-color: #ff0000; /* Cambia el color de fondo para el botón "Cancelar" */
-    color: #fff; /* Cambia el color del texto para el botón "Cancelar" */
-}
+        .custom-button.cancel-button {
+          background-color: gray; /* Cambia el color de fondo para el botón "Cancelar" */
+          color: #fff; /* Cambia el color del texto para el botón "Cancelar" */
+          text-decoration: none; /* Elimina el subrayado del enlace */
+        }
 
-.custom-button {
-    background-color: blue; /* Cambia el color de fondo para el botón "Guardar" */
-    color: #fff; /* Cambia el color del texto para el botón "Guardar" */
-}
-
-.custom-button:hover {
-    background-color: #555; /* Cambio de color de fondo al pasar el cursor sobre ambos botones */
-}
-
-
-
-      
+       .custom-button {
+         background-color: blue; /* Cambia el color de fondo para el botón "Guardar" */
+         color: #fff; /* Cambia el color del texto para el botón "Guardar" */
+        }
     </style>
 </head>
 <body>
 
-<div class="container">
+  <div class="container">
+       <?php
+         session_start(); 
+         include 'db_connect.php';
 
-   
+         if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
+              $id = $_GET['id'];
 
-<?php
-include 'db_connect.php';
+              $stmt = $conn->prepare("SELECT * FROM tbl_categorias WHERE id = :id");
+              $stmt->bindParam(':id', $id);
+              $stmt->execute();
+              $categoria = $stmt->fetch(PDO::FETCH_ASSOC);
+            } else {
+              echo 'ID de categoría no proporcionado.';
+            }
+        ?>
+      <br><br>
+      <form method="POST" action="actualizar_categoria.php">
+          <h2 style="text-align: center;">Categoría</h2><br>
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
-    $id = $_GET['id'];
+           <input type="hidden" name="id" value="<?php echo $categoria['id']; ?>"> <!-- Corregir aquí -->
 
-    $stmt = $conn->prepare("SELECT * FROM tbl_categorias WHERE id = :id");
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
-    $categoria = $stmt->fetch(PDO::FETCH_ASSOC);
-} else {
-    echo 'ID de categoría no proporcionado.';
-}
-?>
-<br><br>
-<form method="POST" action="actualizar_categoria.php">
-     <h2>Editar Categoría</h2><br>
+          <label for="categoria">Categoría:</label>
+          <input type="text" name="categoria" id="categoria" value="<?php echo $categoria['categoria']; ?>" required>
 
-    <input type="hidden" name="id" value="<?php echo $categoria['id']; ?>"> <!-- Corregir aquí -->
+          <label for="creado">Creado por:</label>
+          <input type="text" name="creado" id="creado" value="<?php echo $categoria['creado']; ?>" readonly>
 
-    <label for="categoria">Categoría:</label>
-    <input type="text" name="categoria" id="categoria" value="<?php echo $categoria['categoria']; ?>" required>
+          <label for="fecha_creacion">Fecha de Creación:</label>
+          <input type="text" name="fecha_creacion" id="fecha_creacion" value="<?php echo date("d/m/Y", strtotime($categoria['fecha_creacion'])); ?>" readonly>
 
-    <label for="creado">Creado por:</label>
-    <input type="text" name="creado" id="creado" value="<?php echo $categoria['creado']; ?>" readonly>
+          <label for="modificado">Modificado Por:</label>
+          <input type="text" name="modificado" id="modificado" value="<?php echo $_SESSION['nombre_usuario']; ?>" required>
 
-    <label for="fecha_creacion">Fecha de Creación:</label>
-    <input type="text" name="fecha_creacion" id="fecha_creacion" value="<?php echo date("d/m/Y", strtotime($categoria['fecha_creacion'])); ?>" readonly>
+           <!-- Puedes agregar más campos de edición aquí, si es necesario. -->
+          <br><br>
+          <button type="submit" class="custom-button">Guardar</button>
+            <a href="listar_categorias.php" class="custom-button cancel-button">Cancelar</a>
+       </form>
 
-<label for="fecha_modificacion">Fecha de Modificación:</label>
-<input type="text" name="fecha_modificacion" id="fecha_modificacion" value="<?php echo date("d/m/Y", strtotime($categoria['fecha_modificacion'])); ?>" readonly>
-
-    
-    <label for="modificado">Modificado Por:</label>
-    <input type="text" name="modificado" id="modificado" value="<?php echo $categoria['modificado']; ?>" required>
-    <!-- Puedes agregar más campos de edición aquí, si es necesario. -->
-    <br><br>
-    <button type="submit" class="custom-button">Guardar</button>
-    <a href="listar_categorias.php" class="custom-button cancel-button">Cancelar</a>
-
-</form>
-
-</div>
+    </div>
 </body>
 </html>
