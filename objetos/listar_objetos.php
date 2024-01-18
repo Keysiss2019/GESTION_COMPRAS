e6c0e7ecbe6f8b442694e88d505cf143488e340c
keysiss2019 <keysiss2019@gmail.com>
2024-01-17 18:27:36 -0600

    Filtro en fecha, colores de los botones

--- objetos/listar_objetos.php
+++ objetos/listar_objetos.php
@@ -3,299 +3,252 @@
 <head>
     <meta charset="UTF-8">
     <title>Lista de Objetos</title>
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/860e3c70ee.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-   
-    <!-- Incluye Font Awesome para los íconos -->
-    <script src="https://kit.fontawesome.com/8ea5d7998a.js" crossorigin="anonymous"></script>
-    <!-- Incluye jQuery para el manejo de eventos y AJAX -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-   
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/plug-ins/1.10.25/sorting/datetime-moment.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <style>
-   /* Estilos generales */
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-}
+       body {
+         font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+        }
 
+        .content {
+          margin-left: 10%;
+          transition: margin-left 0.5s;
+          padding: 20px;
+          width: 80%;
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+        }
 
+        /* Estilo para el fondo de los encabezados */
+       .header-background {
+         background-color: #f2f2f2 !important; /* !important para forzar el estilo */
+        
+        }
 
-/* Estilo para el menú lateral retráctil */
-.sidebar {
-    width: 250px;
-    height: 100%;
-    background-color: #1e3a5d;
-    color: #fff;
-    position: fixed;
-    left: -250px;
-    top: 0;
-    transition: left 0.3s;
-    overflow-y: auto;
-    padding: 20px;
-    z-index: 1000;
-}
+        #objetosTable {
+          border-collapse: collapse;
+          width: 100%;
+        }
 
-.sidebar h2 {
-    margin-bottom: 20px;
-}
+        #objetosTable th, #objetosTable td {
+         border: 2px solid #f2f2f2;
+         padding: 8px;
+         text-align: left;
+         background-color: white; /* Fondo blanco en todas las celdas */
+        }
 
-.sidebar ul {
-    list-style-type: none;
-    padding: 0;
-}
+        #objetosTable th {
+          background-color: #f2f2f2;
+        }
 
-.sidebar li {
-    margin-bottom: 10px;
-}
+        #objetosTable tr:nth-child(even) {
+          background-color: #f2f2f2;
+        }
 
-.sidebar a {
-    text-decoration: none;
-    color: #fff;
-}
+       .search-bar {
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          margin-top: 20px;
+        }
 
-.sidebar i {
-    margin-right: 10px;
-}
+       .print-button {
+         background-color: #007bff;
+         color: #fff;
+         padding: 8px 12px;
+         border: none;
+         border-radius: 5px;
+         cursor: pointer;
+         text-decoration: none;
+         margin-right: 10px;
+        }
 
-body.menu-visible .sidebar {
-    left: 0;
-}
+        .back-link {
+          background-color: #007bff;
+          color: #fff;
+           font-size: 14px; /* Tamaño de fuente más pequeño */
+          padding: 8px 16px; /* Ajusta el relleno según sea necesario */
+          text-decoration: none;
+          border-radius: 5px;
+        }
 
-/* Contenido principal */
-.content {
-    margin-left: 50px;
-    padding: 20px;
-}
+        .filter-icon {
+         margin-left: 10px;
+          cursor: pointer;
+        }
 
-/* Estilos para la tabla */
-table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px;
-}
+        #fecha_filtro {
+          margin-left: 10px;
+          padding: 6px;
+        }
 
-th, td {
-    border: 1px solid #ccc;
-    padding: 8px;
-    text-align: left;
-}
+        /* Estilo específico para el botón de regresar */
+      .back-link {
+         background-color: #007bff;
+         color: #fff;
+        }
 
-th {
-    background-color: #007bff;
-    color: #fff;
-}
+        /* Estilo específico para el botón de agregar */
+        .print-button {
+         background-color: #007bff;
+         color: #fff;
+           padding: 8px 10px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          text-decoration: none;
+          margin-right: 20px;
+          margin-top: 18px;
+          margin-left: 0; /* Ajusta el valor de margin-left según sea necesario */
+        }
 
-/* Estilo para el contenedor de la parte superior derecha */
-.top-right {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    margin-right: 20px; /* Ajusta el margen derecho según sea necesario */
-}
+        .actions-cell {
+         white-space: nowrap;
+        }
 
-/* Estilo para el botón de búsqueda (ícono) */
-.search-container button {
-    padding: 5px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-}
-
-/* Estilo para el contenedor de búsqueda */
-.search-container {
-    display: flex;
-    align-items: center;
-    margin-right: 10px; /* Espacio entre el botón de búsqueda y "Crear" */
-}
-
-/* Estilo para el botón "Crear Nuevo Objeto" */
-.create-new-object-link {
-    padding: 5px;
-    text-decoration: none;
-    background-color: #007bff;
-    color: #fff;
-    border-radius: 5px;
-    margin-right: 10px;
-}
-
-/* Estilo para el botón "Descargar CSV" */
-.download-link {
-    padding: 5px 10px;
-    text-decoration: none;
-    background-color: #007bff;
-    color: #fff;
-    border-radius: 5px;
-    display: inline-flex; /* Alinea el ícono y el texto en línea */
-    align-items: center; /* Alinea verticalmente */
-    margin-left: 10px; /* Ajusta el margen izquierdo según sea necesario */
-}
-
-/* Estilo para el ícono de Excel */
-.download-link i {
-    margin-right: 5px;
-}
-
-
-/* Estilos para el título y el ícono de objetos */
-.title-container {
-    display: flex;
-    align-items: center;
-}
-
-h1 {
-    text-align: left;
-    margin: 0;
-    font-size: 36px; /* Ajusta el tamaño del título según sea necesario */
-    margin-right: 10px; /* Espacio entre el título y el icono */
-}
-
-/* Estilo para el ícono de objetos (ajusta la clase según corresponda) */
-.title-container i {
-    font-size: 24px; /* Ajusta el tamaño del icono según sea necesario */
-}
-
-
-
-/* Estilos para los enlaces "Editar" y "Eliminar" */
-a.edit-link {
-    color: #007bff; /* Color azul */
-    text-decoration: none;
-    margin-right: 10px; /* Espacio entre los enlaces */
-}
-
-a.edit-link:hover {
-    text-decoration: underline; /* Subraya el enlace al pasar el mouse */
-}
-
-a.delete-link {
-    color: #d9534f; /* Color rojo */
-    text-decoration: none;
-}
-
-a.delete-link:hover {
-    text-decoration: underline; /* Subraya el enlace al pasar el mouse */
-}
-
-
-/* Añade más estilos para otros colores personalizados según sea necesario */
-
-
+        .actions-cell a {
+         margin-right: 5px;
+        }
     </style>
 </head>
 <body>
-   
-<div class="content">
-    <h1><i class="fas fa-cube"></i> Objetos</h1>
-    <div class="top-right">
-        <!-- Campo de búsqueda y botón -->
-        <div class="search-container">
-            <input type="text" id="search" name="search" placeholder="Buscar por nombre">
-            <button onclick="searchObject()"><i class="fas fa-search"></i></button>
-        </div>
-        <a href="crear_objeto.php" class="create-new-object-link">Crear +</a>
-        <a href="exportar_csv.php" class="download-link">
-            <i class="fas fa-download"></i>Excel
-        </a>
-    </div>
- 
-    <div class="table-container">
-        <table>
+    <div class="content">
+        <?php
+            include '../objetos/db_connect.php';
+
+            if (isset($_GET['busqueda'])) {
+                $busqueda = $_GET['busqueda'];
+
+                if (in_array($busqueda, ['Activo', 'Inactivo', 'Bloqueado', 'Nuevo'])) {
+                    $stmt = $conn->prepare("SELECT * FROM tbl_objetos WHERE estado_objeto = :estado_busqueda");
+                    $stmt->bindValue(':estado_busqueda', $busqueda);
+                } else {
+                    $stmt = $conn->prepare("SELECT * FROM tbl_objetos WHERE id_objeto = :busqueda OR nombre_objeto LIKE :busqueda");
+                    $stmt->bindValue(':busqueda', $busqueda);
+                }
+
+                $stmt->execute();
+                $objetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } else {
+                $stmt = $conn->query("SELECT * FROM tbl_objetos");
+                $objetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        ?>
+
+        <h1><i class="fas fa-cube"></i> Objetos</h1>
+
+        <table id="objetosTable" class="display">
             <thead>
                 <tr>
-                    <th>ID de Objeto</th>
                     <th>Nombre del Objeto</th>
                     <th>Descripción</th>
-                    <th>Fecha de Creación</th>
+                    <th>
+                    <div class="search-bar">
+                    Fecha de Creación
+                        <i id="filterIcon" class="fas fa-filter filter-icon" onclick="toggleFechaFiltro()"></i>
+                        <div id="filterContainer" style="display: none;">
+                          <input type="date" id="fecha_filtro" onchange="filtrarPorFecha()">
+                       </div>
+                    </div>
+                    </th>
                     <th>Fecha de Modificación</th>
                     <th>Creado por</th>
                     <th>Modificado por</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
-            <tbody class="object-list">
-                <?php
-                include '../objetos/db_connect.php';
-                
-                
-
-                // Paginación
-                $registrosPorPagina = 10;
-                $paginaActual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
-                $inicio = ($paginaActual - 1) * $registrosPorPagina;
-
-                // Consulta SQL para obtener la lista de objetos paginada
-                $stmt = $conn->prepare("SELECT * FROM tbl_objetos LIMIT :inicio, :registrosPorPagina");
-                $stmt->bindParam(':inicio', $inicio, PDO::PARAM_INT);
-                $stmt->bindParam(':registrosPorPagina', $registrosPorPagina, PDO::PARAM_INT);
-                $stmt->execute();
-                $objetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                foreach ($objetos as $objeto) {
-                    echo "<tr>";
-                    echo "<td>" . $objeto['ID_OBJETO'] . "</td>";
-                    echo "<td>" . $objeto['NOMBRE_OBJETO'] . "</td>";
-                    echo "<td>" . $objeto['DESCRIPCION'] . "</td>";
-                    echo "<td>" . $objeto['FECHA_CREACION'] . "</td>";
-                    echo "<td>" . $objeto['FECHA_MODIFICACION'] . "</td>";
-                    echo "<td>" . $objeto['CREADO_POR'] . "</td>";
-                    echo "<td>" . $objeto['MODIFICADO_POR'] . "</td>";
-                    echo "<td>";
-                    echo "<a href='editar_objeto.php?id=" . $objeto['ID_OBJETO'] . "' class='edit-link custom-color-1'><i class='fas fa-pencil-alt'></i></a>";
-echo "<a href='eliminar_objeto.php?id=" . $objeto['ID_OBJETO'] . "' class='delete-link custom-color-2'><i class='fas fa-trash'></i></a>";
-echo "<a href='estadisticas_objetos.php?id=" . $objeto['ID_OBJETO'] . "' class='view-link custom-color-3'><i class='fas fa-eye'></i></a>";
-
-
-                    echo "</td>";
-                    echo "</tr>";
-                }
-                ?>
+            <tbody>
+                <?php foreach ($objetos as $objeto) { ?>
+                    <tr>
+                        <td><?php echo $objeto['NOMBRE_OBJETO']; ?></td>
+                        <td><?php echo $objeto['DESCRIPCION']; ?></td>
+                        <td><?php echo $objeto['FECHA_CREACION']; ?></td>
+                        <td><?php echo $objeto['FECHA_MODIFICACION']; ?></td>
+                        <td><?php echo $objeto['CREADO_POR']; ?></td>
+                        <td><?php echo $objeto['MODIFICADO_POR']; ?></td>
+                        <td class="actions-cell">
+                        
+                          <a href='estadisticas_objetos.php?id=<?php echo $objeto['ID_OBJETO']; ?>' class='view-link btn btn-info'><i class='fas fa-eye'></i></a>
+                          <a href='editar_objeto.php?id=<?php echo $objeto['ID_OBJETO']; ?>' class='edit-link btn btn-success'><i class='fas fa-pencil-alt'></i></a>
+                           <a href='eliminar_objeto.php?id=<?php echo $objeto['ID_OBJETO']; ?>' class='delete-link btn btn-danger'><i class='fas fa-trash'></i></a>
+                       </td>
+                    </tr>
+                <?php } ?>
             </tbody>
         </table>
-    </div>
 
-    <!-- Paginación -->
-    <?php
-    // Calcula el número total de registros en la tabla
-    $totalRegistros = $conn->query("SELECT COUNT(*) FROM tbl_objetos")->fetchColumn();
-    $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
+        <div class="search-bar">
+            <a href="crear_objeto.php" class="print-button"><i class="fas fa-plus"></i></a>
+        </div>
 
-    echo '<div class="pagination">';
-    if ($paginaActual > 1) {
-        echo '<a href="?pagina=1">Primera</a>';
-        echo '<a href="?pagina=' . ($paginaActual - 1) . '">Anterior</a>';
-    }
-    for ($i = 1; $i <= $totalPaginas; $i++) {
-        echo '<a href="?pagina=' . $i . '"';
-        if ($i == $paginaActual) {
-            echo ' class="active"';
-        }
-        echo '>' . $i . '</a>';
-    }
-    if ($paginaActual < $totalPaginas) {
-        echo '<a href="?pagina=' . ($paginaActual + 1) . '">Siguiente</a>';
-        echo '<a href="?pagina=' . $totalPaginas . '">Última</a>';
-    }
-    echo '</div>';
-    ?>
-</div>
+        <div class="actions-cell">
+            <a href="../admin/administrar.php" class="back-link"><i class="fas fa-arrow-left"></i> Regresar</a>
+        </div>
 
-<script>
-    function searchObject() {
-        var searchTerm = $('#search').val(); // Obtiene el término de búsqueda del campo de entrada
+        <script>
+            
+            $(document).ready(function() {
+                var table = $('#objetosTable').DataTable({
+                    "dom": 'lBfrtip',
+                    "buttons": ['copy', 'excel', 'pdf', 'print'],
+                    "ordering": false,
+                    "paging": false,
+                    "info": false,
+                    "language": {
+                        "search": "Buscar"
+                    },
+                    "columnDefs": [
+                        {
+                            "targets": 'thead th',
+                            "className": 'header-background'
+                        }
+                    ]
+                });
 
-        // Realiza la solicitud AJAX para buscar objetos por nombre
-        $.ajax({
-            type: 'POST',
-            url: 'buscar_objetos.php', // Crea un archivo PHP para manejar la búsqueda
-            data: { search: searchTerm },
-            success: function (data) {
-                // Actualiza la lista de objetos con los resultados de la búsqueda
-                $('.object-list').html(data);
+              
+            
+                // Configuración del filtro de fecha
+                $('#fecha_filtro').on('change', function () {
+                    var filterValue = $(this).val();
+                    table.column(2).search(filterValue).draw();
+                });
+
+                // Ajustes de estilo
+                setTimeout(function() {
+                    $('div.dataTables_filter').css({
+                        'text-align': 'left',
+                        'margin-top': '10px',
+                        'margin-right': '40px'
+                    });
+
+                    $('.search-bar .print-button').css({
+                        'position': 'absolute',
+                        'right': '1px',
+                        'top': '65px'
+                    });
+                }, 100);
+            });
+
+          function toggleFechaFiltro() {
+             $('#filterContainer').toggle();
             }
-        });
-    }
-</script>
+        </script>
+    </div>
 </body>
 </html>
 
