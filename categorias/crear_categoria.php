4334fd65eb33fc64a07b254d2b8fe1a11110f325
Jnicollordo <150632545+Jnicollordo@users.noreply.github.com>
2024-04-28 16:49:06 -0600

    DISEÑO CATEGORÍAS

--- categorias/crear_categoria.php
+++ categorias/crear_categoria.php
@@ -25,22 +25,25 @@
         }
 
         form {
-            width: 400px;
+            width: 550px;
             padding: 20px;
             background-color: #ddd;
             border: 1px solid #ddd;
             border-radius: 5px;
             margin-bottom: 10px;
+
         }
 
         h2 {
             text-align: center;
+            color: #007BFF; /* Cambiar el color del título a azul */
+           font-weight: bold; /* Hacer el título en negrita */
         }
 
         label {
             display: block;
             margin-top: 10px;
-            font-weight: bold;
+            /*font-weight: bold;*/
         }
 
         input {
@@ -51,13 +54,20 @@
             border-radius: 3px;
         }
 
+        .button-section {
+    clear: both; /* Limpiar el flotado para que los elementos debajo no floten */
+    text-align: center; /* Alinear los botones a la izquierda */
+    margin-top: 20px; /* Espacio superior para separar de las columnas */
+}
+
         .custom-button {
             display: inline-block;
             padding: 10px 20px;
             border: none;
-            border-radius: 3px;
-            font-weight: bold;
+            border-radius: 5px;
+            /*font-weight: bold;*/
             cursor: pointer;
+            text-align: center;
         }
 
         .custom-button.cancel-button {
@@ -67,15 +77,16 @@
         }
 
         .custom-button {
-            background-color: blue;
+            background-color: #007bff;
             color: #fff;
+            font-size: 16px; /* Aumenta ligeramente el tamaño de la letra del botón "Guardar" */
         }
     </style>
 </head>
 <body>
 <div class="container">
     <form action="guardar_categoria.php" method="POST">
-        <h2>Categoría</h2>
+        <h2>CATEGORÍA</h2>
         <label for="categoria">Categoría:</label>
         <input type="text" name="categoria" required>
 
@@ -85,8 +96,11 @@
         <label for="fecha_creacion">Fecha de Creación:</label>
         <input type="text" name="fecha_creacion" value="<?php echo date("Y-m-d"); ?>" readonly>
         <br><br>
+        
+        <div class="button-section">
         <button type="submit" class="custom-button">Guardar</button>
         <a href="listar_categorias.php" class="custom-button cancel-button">Cancelar</a>
+    </div>
     </form>
 </div>
 </body>
--- categorias/editar_categoria.php
+++ categorias/editar_categoria.php
@@ -21,10 +21,8 @@
             padding: 0;
         }
 
-       
-
         form {
-            max-width: 400px;
+            max-width: 550px;
             margin: 0 auto;
             padding: 20px;
             background-color: #ddd;
@@ -36,7 +34,7 @@
         label {
             display: block;
             margin-top: 10px;
-            font-weight: bold;
+            /*font-weight: bold;*/
         }
 
         input {
@@ -46,14 +44,21 @@
             border: 1px solid #ddd;
             border-radius: 3px;
         }
+         
+        .button-section {
+    clear: both; /* Limpiar el flotado para que los elementos debajo no floten */
+    text-align: center; /* Alinear los botones a la izquierda */
+    margin-top: 20px; /* Espacio superior para separar de las columnas */
+}
 
         .custom-button {
           display: inline-block;
           padding: 10px 20px; /* Ajusta el tamaño del botón según tus preferencias */
           border: none;
-          border-radius: 3px;
-          font-weight: bold;
+          border-radius: 5px;
+          /*font-weight: bold;*/
          cursor: pointer;
+         text-align: center; /* Alinear los botones a la izquierda */
         }
 
 
@@ -64,9 +69,17 @@
         }
 
        .custom-button {
-         background-color: blue; /* Cambia el color de fondo para el botón "Guardar" */
-         color: #fff; /* Cambia el color del texto para el botón "Guardar" */
+        background-color: #007bff;
+            color: #fff;
+            font-size: 16px; /* Aumenta ligeramente el tamaño de la letra del botón "Guardar" */
         }
+
+        h2 {
+            text-align: center;
+            color: #007BFF; /* Cambiar el color del título a azul */
+           font-weight: bold; /* Hacer el título en negrita */
+        }
+        
     </style>
 </head>
 <body>
@@ -89,7 +102,7 @@
         ?>
       <br><br>
       <form method="POST" action="actualizar_categoria.php">
-          <h2 style="text-align: center;">Categoría</h2><br>
+          <h2 style="text-align: center;">CATEGORÍA</h2><br>
 
            <input type="hidden" name="id" value="<?php echo $categoria['id']; ?>"> <!-- Corregir aquí -->
 
@@ -107,8 +120,11 @@
 
            <!-- Puedes agregar más campos de edición aquí, si es necesario. -->
           <br><br>
+
+          <div class=button-section>
           <button type="submit" class="custom-button">Guardar</button>
             <a href="listar_categorias.php" class="custom-button cancel-button">Cancelar</a>
+          </div>
        </form>
 
     </div>
